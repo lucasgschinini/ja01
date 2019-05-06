ce67fa4172198a2323aab6633654ab698eeccf91
lucasgschinini <luc.0078@gmail.com>
2019-05-06 12:31:22 -0300

    commiteo del proyecto

---
+++ clase1/src/main/java/lucas/g/educacion/it/Usuario.java
@@ -0,0 +1,27 @@
+package lucas.g.educacion.it;
+
+public class Usuario {
+	
+	private final String id;
+	private final String nombre;
+	
+	public Usuario(String id, String nombre) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+	
+	
+	
+	
+	
+
+}
